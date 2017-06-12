f2b86ccc873c6625e7f8311303637f84934b6153
Natalie N Sanchez <nnsanche@pfe24.nas.nasa.gov>
2017-06-12 14:12:28 -0700

    Fixing wonky colors

--- GM4/sfhistory.py
+++ GM4/sfhistory.py
@@ -4,8 +4,8 @@ import numpy as np
 import pynbody
 
 timesteps = np.loadtxt('timesteps.txt',dtype='str')
-plt.plot([0,0.],[0,0.01],color='FireBrick',label='GM1')
-plt.plot([0,0],[0,0.01],color='SteelBlue',label='GM4')
+plt.plot([0,0.],[0,0.01],color='SteelBlue',label='GM1')
+plt.plot([0,0],[0,0.01],color='FireBrick',label='GM4')
 
 #for j in range(1):#len(timesteps)):
 i = 21
@@ -18,7 +18,7 @@ h1 = h[1]
 pynbody.analysis.angmom.faceon(h1)
 file.physical_units()
 
-sfhist, bins = pynbody.plot.stars.sfh(file,filename='GM4_sfh_xlim'+timesteps[i]+'.pdf',massform=True,color='SteelBlue',legend=True,trange=[0,14])
+sfhist, bins = pynbody.plot.stars.sfh(file,filename='GM4_sfh_xlim'+timesteps[i]+'.pdf',massform=True,color='FireBrick',legend=True,trange=[0,14])
 #pynbody.plot.stars.sfh(h1,filename='GM4_MH_sfh_'+timesteps[i]+'.pdf',massform=False)
     
 file2 = pynbody.load('/nobackupp8/fgoverna/pioneer50h243GM1.1536gs1bwK1BH/pioneer50h243GM1.1536gst1bwK1BH.00'+timesteps[i])
@@ -33,7 +33,7 @@ file.physical_units()
 
 plt.ylim(0,12)
 #    plt.xlim(0,14)
-sfhist2, bins2 = pynbody.plot.stars.sfh(file2,filename='GM1_4_sfh_xlim'+timesteps[i]+'.pdf',massform=True,legend=True,color='FireBrick',trange=[0,14])
+sfhist2, bins2 = pynbody.plot.stars.sfh(file2,filename='GM1_4_sfh_xlim'+timesteps[i]+'.pdf',massform=True,legend=True,color='SteelBlue',trange=[0,14])
 #plt.xlim(0,15)
 plt.legend(loc=2)
 plt.savefig('GM1_4_sfh_'+timesteps[i]+'.pdf')
