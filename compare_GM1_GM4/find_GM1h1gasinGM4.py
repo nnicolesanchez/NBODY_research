177f8959e64a8257cb92b1e32dd6a12a57f07595
Natalie N Sanchez <nnsanche@pfe24.nas.nasa.gov>
2017-06-27 14:50:41 -0700

    Added temperature and metallicity color coded plots

--- compare_GM1_GM4/find_GM1h1gasinGM4.py
+++ compare_GM1_GM4/find_GM1h1gasinGM4.py
@@ -32,7 +32,7 @@ GM1_sim.physical_units()
 
 # np.in1d is one of the most useful things I have ever learned
 # matched items in different arrays
-GM1h1z0iords = np.random.choice(GM1h1z0iords,size=200000)
+GM1h1z0iords = np.random.choice(GM1h1z0iords,size=5000)
 
 GM4_GM1h1z0_gasmask  = np.in1d(GM4_sim.g['iord'],GM1h1z0iords,assume_unique=False)
 GM4_GM1h1z0_starmask = np.in1d(GM4_sim.s['igasorder'],GM1h1z0iords,assume_unique=False) 
@@ -46,16 +46,25 @@ print(len(GM1_sim.g['iord'][GM1h1z0_gasmask]))
 #test2 = np.random.choice(GM1_sim.g['pos'],size=10000)
 
 GM4_gaspos = np.array(GM4_sim.g['pos'][GM4_GM1h1z0_gasmask])
+GM4_gastemp = np.array(GM4_sim.g['temp'][GM4_GM1h1z0_gasmask])
+GM4_gasmetal = np.array(GM4_sim.g['Metalsdot'][GM4_GM1h1z0_gasmask])
 GM4_gasR = (GM4_gaspos[:,0]**2 + GM4_gaspos[:,1]**2 + GM4_gaspos[:,2]**2)**0.5
 
 GM1_gaspos = np.array(GM1_sim.g['pos'][GM1h1z0_gasmask])
+GM1_gastemp = np.array(GM1_sim.g['temp'][GM1h1z0_gasmask])
+GM1_gasmetal = np.array(GM1_sim.g['Metalsdot'][GM1h1z0_gasmask])
+print(GM1_sim.g['Metalsdot'])
 GM1_gasR = (GM1_gaspos[:,0]**2 + GM1_gaspos[:,1]**2 + GM1_gaspos[:,2]**2)**0.5
 
 matchgas_mask = np.in1d(GM4_sim.g['iord'][GM4_GM1h1z0_gasmask],GM1_sim.g['iord'][GM1h1z0_gasmask])
+new_GM4_gastemp = np.array(GM4_gastemp[matchgas_mask])
+new_GM4_gasmetal = np.array(GM4_gasmetal[matchgas_mask])
 new_GM4_gaspos = np.array(GM4_gaspos[matchgas_mask])
 new_GM4_gasR = (new_GM4_gaspos[:,0]**2 + new_GM4_gaspos[:,1]**2 + new_GM4_gaspos[:,2]**2)**0.5
 
 matchgas_mask1 = np.in1d(GM1_sim.g['iord'][GM1h1z0_gasmask],GM4_sim.g['iord'][GM4_GM1h1z0_gasmask])
+new_GM1_gastemp = np.array(GM1_gastemp[matchgas_mask1])
+new_GM1_gasmetal = np.array(GM1_gasmetal[matchgas_mask1])
 new_GM1_gaspos = np.array(GM1_gaspos[matchgas_mask1])
 new_GM1_gasR = (new_GM1_gaspos[:,0]**2 + new_GM1_gaspos[:,1]**2 + new_GM1_gaspos[:,2]**2)**0.5
 
@@ -65,18 +74,13 @@ new_GM1_gasR = (new_GM1_gaspos[:,0]**2 + new_GM1_gaspos[:,1]**2 + new_GM1_gaspos
 #plt.savefig('compareR_GM1_GM4.pdf')
 #plt.show()
 
-plt.plot(new_GM4_gaspos[:,0],new_GM4_gaspos[:,1],linestyle='None',markersize=1,marker='o')
-plt.xlabel('GM4 x [kpc]')
-plt.ylabel('GM4 y [kpc]')
-plt.ylim(-2500,1500)
-plt.xlim(-3000,2000)
-plt.savefig('GM4_xy.pdf')
-plt.show()
+#plt.plot(new_GM4_gaspos[:,0],new_GM4_gaspos[:,1],linestyle='None',markersize=1,marker='o')
+np.savetxt('GM4_GM1matchedgas_posxyz.txt',new_GM4_gaspos)
+np.savetxt('GM4_GM1matchedgas_temp.txt',new_GM4_gastemp)
+np.savetxt('GM4_GM1matchedgas_metal.txt',new_GM4_gasmetal)
+np.savetxt('GM1matchedgas_posxyz.txt',new_GM1_gaspos)
+np.savetxt('GM1matchedgas_temp.txt',new_GM1_gastemp)
+np.savetxt('GM1matchedgas_metal.txt',new_GM1_gasmetal)
 
-plt.plot(new_GM1_gaspos[:,0],new_GM1_gaspos[:,1],linestyle='None',markersize=1,marker='o')
-plt.xlabel('GM1 x [kpc]')
-plt.ylabel('GM1 y [kpc]')
-plt.ylim(-2500,1500)
-plt.xlim(-3000,2000)
-plt.savefig('GM1_xy.pdf')
-plt.show()
+
+
