30b061bd278a7997fb8c0e6ced1441b0460766e3
Natalie N Sanchez <nnsanche@pfe26.nas.nasa.gov>
2017-09-18 22:34:36 -0700

    Added zoomin SFH and added stellar mass to MH vs CGM mass plots

--- properties/sfhistory.py
+++ properties/sfhistory.py
@@ -1,32 +1,35 @@
 # This script plots the star formation history for the GM
-# suite for h243
+# suite for h243 (all on one plot)
 import matplotlib.pyplot as plt
 import numpy as np
 import pynbody
 
-#plt.plot([0,0.],[0,0.01],color='SteelBlue',label='GM1')
-#plt.plot([0,0],[0,0.01],color='FireBrick',label='GM4')
 
+# Information for loading all available sims P0 - GM7
 sims = ['/nobackupp8/fgoverna/pioneer50h243.1536g1bwK1BH/pioneer50h243.1536gst1bwK1BH.004096','/nobackupp8/fgoverna/pioneer50h243GM1.1536gs1bwK1BH/pioneer50h243GM1.1536gst1bwK1BH.004096','/nobackupp8/fgoverna/pioneer50h243GM4.1536gst1bwK1BH/pioneer50h243GM4.1536gst1bwK1BH.004096','/nobackup/nnsanche/pioneer50h243GM5.1536gst1bwK1BH/pioneer50h243GM5.1536gst1bwK1BH.004096','/nobackupp8/fgoverna/pioneer50h243GM6.1536gst1bwK1BH/pioneer50h243GM6.1536gst1bwK1BH.004096','/nobackup/nnsanche/pioneer50h243GM7.1536gst1bwK1BH/pioneer50h243GM7.1536gst1bwK1BH.004096']
 labels = ['P0','GM1','GM4','GM5','GM6','GM7']
 colors = ['DodgerBlue','SteelBlue','FireBrick','IndianRed','Salmon','Orange']
 
+# Create sfh data arrays (also save 
 for i in range(len(sims)):
     print(labels[i])
     sim = pynbody.load(sims[i])
     h = sim.halos()
     h1 = h[1]
     pynbody.analysis.halo.center(h1,mode='ssc')
-
-
     pynbody.analysis.angmom.faceon(h1)
     sim.physical_units()
 
+    # Get sfh info for whole sim; change "sim" below to "h1" for Main Halo sfh
     sfhist, bins = pynbody.plot.stars.sfh(sim,filename=labels[i]+'sfh_xlim.pdf',massform=True,color=colors[i],legend=True,trange=[0,14],label=labels[i])
-    #pynbody.plot.stars.sfh(h1,filename='GM4_MH_sfh_'+timesteps[i]+'.pdf',massform=False)
 
+
+    # Save for easier plot making later (tho this script doesn't take that long to run)
+    # Good practice to keep in place though
     np.savetxt(labels[i]+'_sfhistory_bins.txt', np.transpose([sfhist, bins[:-1]]))
 
+# For individual labels instead of legend (bc Fabio asked. Shrug. I guess it looks better?)
+# Remember to "unset" legend when using these instead
 #plt.text(10,11,'P0',color=colors[0])
 #plt.text(8,7.5,'GM1',color=colors[1])
 #plt.text(4,1,'GM4',color=colors[2])
@@ -35,6 +38,7 @@ for i in range(len(sims)):
 #plt.text(1,3,'GM7',color=colors[5])
 
 plt.ylim(0,12)
+#plt.xlim(5,7)
 plt.legend(loc=2)
 plt.savefig('P0-GM7_sfh.pdf')
 plt.show()
