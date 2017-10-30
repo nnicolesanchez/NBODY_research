91583dd86dc0c0f0629e0a8ede460fca911cd96a
Natalie N Sanchez <nnsanche@pfe21.nas.nasa.gov>
2017-10-30 10:30:43 -0700

    Galaxy labels are important

--- ioniz_species/OVI_frompontzen.py
+++ ioniz_species/OVI_frompontzen.py
@@ -16,7 +16,7 @@ def N_OVI(f):
     return f.gas['rho'].in_units('g cm**-3')*ovi*f.gas['OxMassFrac']/(16*m_p)
 
     
-k = 4
+k = 0
 sim = ['/nobackupp8/fgoverna/pioneer50h243.1536g1bwK1BH/pioneer50h243.1536gst1bwK1BH.004096','/nobackupp8/fgoverna/pioneer50h243GM1.1536gs1bwK1BH/pioneer50h243GM1.1536gst1bwK1BH.004096','/nobackupp8/fgoverna/pioneer50h243GM4.1536gst1bwK1BH/pioneer50h243GM4.1536gst1bwK1BH.004096','/nobackup/nnsanche/pioneer50h243GM5.1536gst1bwK1BH/pioneer50h243GM5.1536gst1bwK1BH.004096','/nobackupp8/fgoverna/pioneer50h243GM6.1536gst1bwK1BH/pioneer50h243GM6.1536gst1bwK1BH.004096','/nobackup/nnsanche/pioneer50h243GM7.1536gst1bwK1BH/pioneer50h243GM7.1536gst1bwK1BH.004096']
 labels = ['P0','GM1','GM4','GM5','GM6','GM7']
 
@@ -96,6 +96,7 @@ plt.ylabel(r'N$_{OVI}$')
 plt.xlabel(r'$b$ [kpc]')
 plt.ylim(14,17.5)
 plt.xlim(0,200)
+plt.text(140,17.25,str(labels[k]))
 plt.legend()
 plt.savefig(labels[k]+'_NOVI_b.pdf')
 plt.show()
