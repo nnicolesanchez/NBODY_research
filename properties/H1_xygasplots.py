936f2898d793834d91ee70af6914e04380cd16e0
Natalie N Sanchez <nnsanche@pfe27.nas.nasa.gov>
2017-11-14 17:54:57 -0800

    Tvir across time

--- properties/H1_xygasplots.py
+++ properties/H1_xygasplots.py
@@ -17,7 +17,7 @@ plt.rc('lines', lw=2)
 plt.rc('axes', lw=1, labelsize=12)
 
 if len(sys.argv) == 1:
-    print('No galaxy selected. Current options: P0, GM1, GM4')
+    print('No galaxy selected. Current options: P0, GM1, GM4, GM5, GM6, GM7')
     print('Syntax: "GM_xygasplots.py GM1"')
     quit()
 else:
@@ -31,17 +31,23 @@ else:
         sim = pynbody.load('/nobackup/nnsanche/pioneer50h243GM5.1536gst1bwK1BH/pioneer50h243GM5.1536gst1bwK1BH.004096')
     elif (str(sys.argv[1]) == 'GM6'):
         sim = pynbody.load('/nobackupp8/fgoverna/pioneer50h243GM6.1536gst1bwK1BH/pioneer50h243GM6.1536gst1bwK1BH.004096')
+    elif (str(sys.argv[1]) == 'GM7'):
+        sim = pynbody.load('/nobackup/nnsanche/pioneer50h243GM7.1536gst1bwK1BH/pioneer50h243GM7.1536gst1bwK1BH.004096')
 
     else :
-        print('Not a valid option. Current options: P0, GM1, GM4')
+        print('Not a valid option. Current options: P0, GM1, GM4, GM5, GM6, GM7')
         print('Syntax: "GM_xygasplots.py GM1"')
         quit()    
 
     name = str(sys.argv[1])
     print(name+' simulation at z = ','%.2f' % sim.properties['z'] )
     R_vir = np.loadtxt('../'+name+'/'+name+'_mainhalo.stat',dtype=float,skiprows=1,usecols=6,unpack=True)
-    print('Virial radius:',float(R_vir))
-    R_vir = float(R_vir)
+
+if (str(sys.argv[1]) == 'GM7'):
+    R_vir = h1.properties['Rvir'] 
+
+print('Virial radius:',float(R_vir))
+R_vir = float(R_vir)
 
 h = sim.halos()
 h1 = h[1]
@@ -54,7 +60,7 @@ sim.g['rho_in_nhcm'] = sim.g['rho'].in_units('g cm**-3') / m_h
 
 
 # Plotting face on
-pynbody.plot.sph.image(sim.g, qty='rho_in_nhcm' , width=2*R_vir, cmap='jet', show_cbar=False, vmin=10**-6, vmax=10**1)
+pynbody.plot.sph.image(sim.g, qty='rho_in_nhcm' , width=2*270, cmap='jet', show_cbar=False, vmin=10**-6, vmax=10**1)
 fig = plt.gcf()
 ax = fig.gca()
 
@@ -65,9 +71,13 @@ cbar = plt.colorbar()
 cbar.set_label(r'n$_H$ [cm$^{-3}$]')
 plt.text(-245,220,name, color='White')
 plt.text(185,-240,'z = 0',color='White')
+#plt.xlim(275,275)
+#plt.ylim(275,275)
 plt.savefig(name+'_gasdensity.pdf')
 plt.show()
+plt.clf()
 
+quit()
 # Plotting temperature map
 pynbody.plot.sph.image(sim.g, qty='temp', width=2*R_vir, cmap='jet', show_cbar=False,vmin=10**3, vmax=10**7)
 fig = plt.gcf()
